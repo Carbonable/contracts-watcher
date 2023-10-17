8d8e75bae527950818bf1f599cf726b80c1ae1dc
François Schuers <francois.schuers@gmail.com>
2023-10-17 12:35:27 +0200

    fix: link and charts UI

--- app/components/common/LinkComponent.tsx
+++ app/components/common/LinkComponent.tsx
@@ -2,9 +2,12 @@ import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline"
 
 export default function LinkComponent({ href, title }: { href: string, title: string }) {
     return (
-        <div className="text-ellipsis overflow-hidden text-neutral-300">
-            <span>{title}: </span>
-            <a href={href} target="_blank" rel="noreferrer" className="text-neutral-100 hover:text-neutral-50">{href}</a>
+        <div className="text-ellipsis overflow-hidden text-neutral-300 flex items-center">
+            <div>{title}: </div>
+            <div className="text-neutral-100 hover:text-neutral-50 flex items-center ml-2">
+                <a href={href} target="_blank" rel="noreferrer" className="hover:underline">{href}</a>
+                <ArrowTopRightOnSquareIcon className="ml-1 w-4 h-4 cursor-pointer" />
+            </div>
         </div>
     )
 }
@@ -14,10 +17,9 @@ export function ContractLinkComponent({ href, title, address }: { href: string, 
         <div className="text-ellipsis overflow-hidden text-neutral-300 flex items-center">
             <div>{title}: </div>
             <div className="text-neutral-100 hover:text-neutral-50 flex items-center ml-2">
-                <a href={href} target="_blank" rel="noreferrer">{address}</a>
+                <a href={href} target="_blank" rel="noreferrer" className="hover:underline">{address}</a>
                 <ArrowTopRightOnSquareIcon className="ml-1 w-4 h-4 cursor-pointer" />
             </div>
-            
         </div>
     )
 }
