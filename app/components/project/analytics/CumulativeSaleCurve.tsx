68d5af7d8c8e81b3b67f347033fb0e44ad7f56fd
François Schuers <francois.schuers@gmail.com>
2023-10-17 12:35:27 +0200

    fix: link and charts UI

--- app/components/project/analytics/CumulativeSaleCurve.tsx
+++ app/components/project/analytics/CumulativeSaleCurve.tsx
@@ -100,7 +100,7 @@ export default function CumulativeSaleCurve() {
                                 <stop offset="95%" stopColor="#334566" stopOpacity={0}/>
                             </linearGradient>
                             <linearGradient id="colorFutureCumSalePrice" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#334566" stopOpacity={0.4}/>
+                                <stop offset="5%" stopColor="#334566" stopOpacity={0.3}/>
                                 <stop offset="95%" stopColor="#334566" stopOpacity={0}/>
                             </linearGradient>
                         </defs>
--- app/components/project/analytics/UpdatedPriceCurve.tsx
+++ app/components/project/analytics/UpdatedPriceCurve.tsx
@@ -99,7 +99,7 @@ export default function UpdatedPriceCurve() {
                                 <stop offset="95%" stopColor="#877B44" stopOpacity={0}/>
                             </linearGradient>
                             <linearGradient id="colorFutureUpdatedPrice" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#877B44" stopOpacity={0.4}/>
+                                <stop offset="5%" stopColor="#877B44" stopOpacity={0.3}/>
                                 <stop offset="95%" stopColor="#877B44" stopOpacity={0}/>
                             </linearGradient>
                         </defs>
