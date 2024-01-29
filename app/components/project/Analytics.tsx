da9ae95df22d34bdf3ed55cb4f5ea732460e3644
François Schuers <francois.schuers@gmail.com>
2024-01-29 13:36:13 +0100

    feat: add forecast curves

--- app/components/project/Analytics.tsx
+++ app/components/project/Analytics.tsx
@@ -4,6 +4,7 @@ import CumulativeSaleCurve from './analytics/CumulativeSaleCurve';
 import UpdatedPriceCurve from './analytics/UpdatedPriceCurve';
 import APRCurve from './analytics/APRCurve';
 import { useConfig } from '~/root';
+import ForecastCurve from './analytics/ForecastCurve';
 
 export default function Analytics() {
     const { projectAbi, yielderAbi } = useProjectAbis();
@@ -19,6 +20,7 @@ export default function Analytics() {
         <>
             <CumulativeAbsorptionCurve />
             <UpdatedPriceCurve />
+            <ForecastCurve />
             { !isPublic && <CumulativeSaleCurve /> }
             { !isPublic && <APRCurve /> }
         </>
