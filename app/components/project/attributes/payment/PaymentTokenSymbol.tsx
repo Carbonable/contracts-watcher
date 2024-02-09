14cd5c5440bcb977da54516e60e4ad87d6812728
François Schuers <francois.schuers@gmail.com>
2024-02-09 17:00:42 +0100

    fix: payment token in cairo 1

--- app/components/project/attributes/payment/PaymentTokenSymbol.tsx
+++ app/components/project/attributes/payment/PaymentTokenSymbol.tsx
@@ -2,6 +2,7 @@ import { useContractRead } from "@starknet-react/core";
 import LabelComponent from "~/components/common/LabelComponent";
 import { num, type Abi, shortString } from "starknet";
 import LoadingAndError from "~/components/common/LoadingAndError";
+import { useEffect, useState } from "react";
 
 export default function PaymentTokenSymbol({ abi, address }: { abi: Abi, address: string}) {
 
@@ -11,14 +12,30 @@ export default function PaymentTokenSymbol({ abi, address }: { abi: Abi, address
         functionName: 'symbol'
     });
 
+    const [symbol, setSymbol] = useState<string>("");
+
     const title = "Payment token";
 
-    if (isLoading || isError || data === undefined || typeof data !== 'object') {
+    useEffect(() => {
+        if (data === undefined) { return; }
+
+        if (typeof data === 'bigint') {
+            setSymbol(shortString.decodeShortString(num.toHex(data)).toString());
+            console.log(shortString.decodeShortString(num.toHex(data)).toString());
+        }
+
+        if (typeof data === 'object') {
+            setSymbol(shortString.decodeShortString(num.toHex((data as any).symbol)).toString());
+            console.log(shortString.decodeShortString(num.toHex((data as any).symbol)).toString());
+        }
+    }, [data]);
+
+    if (isLoading || isError || data === undefined || (typeof data !== 'object' && typeof data !== 'bigint')) {
         return (
             <LoadingAndError
                 title={title}
                 isLoading={isLoading}
-                isError={isError || (data === undefined || typeof data !== 'object')}
+                isError={isError || (data === undefined || (typeof data !== 'object' && typeof data !== 'bigint'))}
                 error={error}
             />
         )
@@ -27,8 +44,7 @@ export default function PaymentTokenSymbol({ abi, address }: { abi: Abi, address
     return (
         <LabelComponent
             title="Payment token"
-            value={shortString.decodeShortString(num.toHex((data as any).symbol)).toString()}
-
+            value={symbol}
         />
     )
 }
